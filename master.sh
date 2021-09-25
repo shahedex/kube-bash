e3a37726686249be82c92cb13e7d42c97cb720b7
Shahed Mehbub <shahed@theclinician.com>
2021-09-25 16:43:41 +0600

    add master and node init

---
+++ master.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "This script is written to work with Ubuntu 18.04"
+sleep 3
+echo
+echo "Disable swap until next reboot"
+echo 
+sudo swapoff -a
+
+echo "Update the local node"
+sudo apt-get update && sudo apt-get upgrade -y
+echo
+echo "Install Docker"
+sleep 3
+
+sudo apt-get install -y docker.io
+echo
+echo "Install kubeadm, kubelet, and kubectl"
+sleep 3
+
+sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list"
+
+sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
+
+sudo apt-get update
+
+sudo apt-get install -y kubeadm=1.20.1-00 kubelet=1.20.1-00 kubectl=1.20.1-00
+
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo
+echo "Installed - now to get Calico Project network plugin"
+
+sleep 3
+
+sudo kubeadm init --kubernetes-version 1.20.1 --pod-network-cidr 192.168.0.0/16
+
+sleep 5
+
+echo "Running the steps explained at the end of the init output"
+
+mkdir -p $HOME/.kube
+
+sleep 2
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sleep 2
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "Apply Calico network plugin from ProjectCalico.org"
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+echo
+echo
+sleep 3
+echo "You should see this node in the output below"
+echo "It can take up to a mintue for node to show Ready status"
+echo
+kubectl get node
+echo
+echo
+echo "Script finished. Move to the next step"
